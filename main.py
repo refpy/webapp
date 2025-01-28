8256121fb1f58de55d34d139bafff7df1d265a56
50317506+ismaelW12@users.noreply.github.com <ismael.ripoll@subseaenergies.com>
2025-01-28 12:05:17 +0000

    Updates

--- main.py
+++ main.py
@@ -13,43 +13,78 @@ with st.container():
 
 # Sidebar with a slider for values ranging from 1 to 10
 st.sidebar.image("logo.png")
-st.sidebar.title("Number of Pipelines")
-slider_value = st.sidebar.slider("Select a value", min_value=1, max_value=5, value=2)
+st.sidebar.title("Pipeline Design App")
+slider_value = st.sidebar.slider("Number of Cases", min_value=1, max_value=5, value=2)
 
 # Sample data for DataFrame
 data = {
-    'Outer Diameter': np.full(slider_value, np.nan),
-    'Wall Thickness': np.full(slider_value, np.nan),
-    'Coating Thickness': np.full(slider_value, np.nan)
+    'OD': np.full(slider_value, np.nan),
+    'WT': np.full(slider_value, np.nan),
+    'ca': np.full(slider_value, np.nan),
+    'Material': np.full(slider_value, 1),
+    'SMYS': np.full(slider_value, np.nan),
+    'SMTS': np.full(slider_value, np.nan),
+    'Temperature': np.full(slider_value, np.nan),
+    'alpha_u': np.full(slider_value, np.nan),
 }
 df = pd.DataFrame(data)
 
 # Columns
 st.write("### Inputs")
-col1, col2, col3 = st.columns(3)
+col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
 with col1:
     for index, row in df.iterrows():
-        df.at[index, 'Outer Diameter'] = st.number_input(f'Outer Diameter for Row{index+1}',
-                                                         value=row['Outer Diameter'],
-                                                         format="%.4f")
+        df.at[index, 'OD'] = st.number_input(f'OD{index+1}',
+                                             value=row['OD'],
+                                             format="%.4f")
 with col2:
     for index, row in df.iterrows():
-        df.at[index, 'Wall Thickness'] = st.number_input(f'Wall Thickness for Row{index+1}',
-                                                         value=row['Wall Thickness'],
-                                                         format="%.4f")
+        df.at[index, 'WT'] = st.number_input(f'WT{index+1}',
+                                             value=row['WT'],
+                                             format="%.4f")
 with col3:
     for index, row in df.iterrows():
-        df.at[index, 'Coating Thickness'] = st.number_input(f'Coating Thickness for Row{index+1}',
-                                                            value=row['Coating Thickness'],
-                                                            format="%.4f")
+        df.at[index, 'ca'] = st.number_input(f'ca{index+1}',
+                                             value=row['ca'],
+                                             format="%.4f")
+with col4:
+    for index, row in df.iterrows():
+        df.at[index, 'Material'] = st.number_input(f'Material{index+1}',
+                                                   value=row['Material'],
+                                                   format="%.4f")
+with col5:
+    for index, row in df.iterrows():
+        df.at[index, 'SMYS'] = st.number_input(f'SMYS{index+1}',
+                                               value=row['SMYS'],
+                                               format="%.4f")
+with col6:
+    for index, row in df.iterrows():
+        df.at[index, 'SMTS'] = st.number_input(f'SMTS{index+1}',
+                                               value=row['SMTS'],
+                                               format="%.4f")
+with col7:
+    for index, row in df.iterrows():
+        df.at[index, 'Temperature'] = st.number_input(f'Temperat.{index+1}',
+                                                      value=row['Temperature'],
+                                                      format="%.4f")
+with col8:
+    for index, row in df.iterrows():
+        df.at[index, 'alpha_u'] = st.number_input(f'alpha_u{index+1}',
+                                                  value=row['alpha_u'],
+                                                  format="%.4f")
 
-df['Inner Diameter'] = df.apply(
-    lambda x: rp.inner_diameter(x['Outer Diameter'], x['Wall Thickness']),
+df['Burst Pressure'] = df.apply(
+    lambda x: rp.burst_pressure(x['OD'], x['WT'], x['ca'],
+                                np.full(1, x['Material']), x['SMYS'], x['SMTS'],
+                                np.full(1, x['Temperature']), x['alpha_u'])[0],
     axis = 1)
-df['Total Outer Diameter'] = df.apply(
-    lambda x: rp.total_outer_diameter(x['Outer Diameter'], x['Coating Thickness']),
-    axis = 1)
+df['Burst Pressure'] = df['Burst Pressure'] / 1.0E+06
 
 # Outputs
-st.write("### Outputs")
+st.write("### Outputs - Summary Table")
 st.dataframe(df)
+
+# Create distplot
+st.write("### Outputs - Plot")
+st.bar_chart(df['Burst Pressure'],
+             x_label = 'Case Number', y_label = 'Burst Pressure (MPa)')
